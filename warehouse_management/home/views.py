8920f02d97255ebf4a51827d36a48e3b2baedd95
XeoN365 <xee3oon@gmail.com>
2024-08-11 00:11:16 +0100

    typos

--- warehouse_management/home/views.py
+++ warehouse_management/home/views.py
@@ -5,12 +5,14 @@ def index(request):
     names_of_apps = [
         "Inventory",
         "Orders",
-        "Authenticate",
     ]
-    links_to_apps = ["inventory_list", "order_list", "user_login"]
+    links_to_apps = ["inventory_list", "order_list"]
     if request.user.is_authenticated:
         names_of_apps.append("Logout")
         links_to_apps.append("user_logout")
+    else:
+        names_of_apps.append("Login")
+        links_to_apps.append("user_login")
     apps = zip(names_of_apps, links_to_apps)
     print(names_of_apps)
     return render(
