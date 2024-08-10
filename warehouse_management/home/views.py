90401f3847cadbfc55f6e38397b96534708ccea5
XeoN365 <xee3oon@gmail.com>
2024-08-11 00:10:32 +0100

    added logout button

--- warehouse_management/home/views.py
+++ warehouse_management/home/views.py
@@ -8,6 +8,9 @@ def index(request):
         "Authenticate",
     ]
     links_to_apps = ["inventory_list", "order_list", "user_login"]
+    if request.user.is_authenticated:
+        names_of_apps.append("Logout")
+        links_to_apps.append("user_logout")
     apps = zip(names_of_apps, links_to_apps)
     print(names_of_apps)
     return render(
